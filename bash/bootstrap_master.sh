9aa552bbe7a4e582841e2966db82f5f3fa7952f6
Govindarajan V <govindarajanv@gmail.com>
2022-11-13 11:18:49 +0530

    bash based scripts

---
+++ bash/bootstrap_master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "[TASK 1] Initialize Kubernetes Cluster"
+kubeadm init --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p /home/cloud_user/.kube
+cp -i /etc/kubernetes/admin.conf /home/cloud_user/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+
+echo "[TASK 3] Deploy Flannel network"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml >/dev/null 2>&1
+
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null

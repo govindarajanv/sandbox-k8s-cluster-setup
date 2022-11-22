7976823185a34afb2c7864ad203aa9b32f567c77
Govindarajan V <govindarajanv@gmail.com>
2022-11-22 15:53:24 +0530

    Update bootstrap_master.sh

--- bash/bootstrap_master.sh
+++ bash/bootstrap_master.sh
@@ -16,3 +16,9 @@ kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubuser
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm -y
